c219ab367d469a4cb1892313892ca4cbc4630460
Shauket, Muhammad <muhammad.shauket@rakuten.com>
2018-07-09 12:25:11 +0800

    first commit

---
+++ DataControllerViewControllers/Viewcontroller2.swift
@@ -0,0 +1,9 @@
+//
+//  Viewcontroller2.swift
+//  DataControllerViewControllers
+//
+//  Created by Shauket, Muhammad on 2018/07/09.
+//  Copyright © 2018 Shauket, Muhammad. All rights reserved.
+//
+
+import Foundation
